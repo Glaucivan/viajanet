f25f23b505b4fdf1436652e2dcb68612669366c5
Glaucivan Pereira <49631118+Glaucivan@users.noreply.github.com>
2019-04-15 02:23:07 -0300

    Add files via upload

---
+++ responsivo/index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>ViajaNet - Passagens Aéreas com economia garantida!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/x-icon" href="https://www.viajanet.com.br/static/assets/companies/ViajaNet/img/favicon.ico"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/js/jquery-ui-1.10.3.custom.css" type="text/css">
+    <link rel="stylesheet" href="assets/jquery-ui.css" type="text/css">
+	<script language="javascript" src="assets/jquery-1.9.1.js"></script>
+	<script language="javascript" src="assets/jquery-ui-1.10.3.custom.js"></script>
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#"><img src="https://www.viajanet.com.br/static/assets/platform/img/logo.svg;wa-prod647dd6c977ad3189"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="https://www.viajanet.com.br/passagens-aereas">Passagens Aéreas<span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="https://www.viajanet.com.br/pacotes-viagens">Pacotes de Viagem</a>
+				</li><span class="badge badge-danger">Novo</span>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="https://hoteis.viajanet.com.br/?lang=pt-br;selected_currency=BRL;label=ViajaNet_Home_Menu_Institucional">Hotéis</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="http://carros.viajanet.com.br/#/">Carros</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="https://www.viajanet.com.br/passagens-aereas/quandoviajar">QuandoViajar</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="https://atendimento.viajanet.com.br/hc/pt-br">Atendimento</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="https://www.viajanet.com.br/shop/authentication/#/sign-in">Acesse ou Crie uma conta</a>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<div class="btn-group" role="group" aria-label="Ligue">
+						<div class="btn-group" role="group">
+						<a id="btnGroupDrop1" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Ligue
+						</a>
+							<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+								<a class="dropdown-item" href="#">4007-1510<br>Regiões Metropolitanas</a>
+								<a class="dropdown-item" href="#">(11) 4007-1510<br>Demais Regiões</a>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>	
+		
+	</nav>
+
+	<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="assets/images/back1.jpg" alt="Paisagem 1">
+			</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="assets/images/back2.jpg" alt="Paisagem 2">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="assets/images/back3.jpg" alt="Paisagem 3">
+		</div>
+		</div>
+	</div>
+	
+	<div class="container"id="cards">
+	<br>
+	<h3>Passagens Nacionais</h3>
+	<br>
+		<div class="row">
+			<div class="col">
+				<div class="card">
+					<img class="card-img-top" src="https://www.viajanet.com.br/static/assets/platform/img/catalog/RIO/rio-de-janeiro-nova.jpg" alt="Promoção de São Paulo para Rio de Janeiro">
+						<div class="card-body">
+							<p class="card-text">Promoção de São Paulo para Rio de Janeiro</p>
+							<a href="https://www.viajanet.com.br/busca/passagens/voos#/SAO/RIO/RT/01-11-2019/03-11-2019/-/-/-/1/0/0/-/-/BRL/-" class="btn btn-primary">Comprar agora!</a>
+						</div>
+				</div>
+			</div>
+		<div class="col">
+			<div class="card">
+				<img class="card-img-top" src="https://www.viajanet.com.br/static/assets/platform/img/catalog/MAO/manaus.jpg" alt="Promoção de São Paulo para Manaus">
+					<div class="card-body">
+						<p class="card-text">Promoção de São Paulo para Manaus</p>
+						<a href="https://www.viajanet.com.br/busca/passagens/voos#/SAO/MAO/RT/12-04-2019/15-04-2019/-/-/-/1/0/0/-/-/BRL/-" class="btn btn-primary">Comprar agora!</a>
+					</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card">
+				<img class="card-img-top" src="https://www.viajanet.com.br/static/assets/platform/img/catalog/FLN/florianopolis.jpg" alt="Promoção de São Paulo para Florianópolis">
+					<div class="card-body">
+						<p class="card-text">Promoção de São Paulo para Florianópolis</p>
+						<a href="https://www.viajanet.com.br/busca/passagens/voos#/SAO/FLN/RT/31-05-2019/10-06-2019/-/-/-/1/0/0/-/-/BRL/-" class="btn btn-primary">Comprar agora!</a>
+					</div>
+			</div>
+		</div>
+			<div class="col">
+				<div class="card">
+					<img class="card-img-top" src="https://www.viajanet.com.br/static/assets/platform/img/catalog/BPS/porto-seguro.jpg" alt="Promoção de São Paulo para Porto Seguro">
+						<div class="card-body">
+							<p class="card-text">Promoção de São Paulo para Porto Seguro</p>
+							<a href="https://www.viajanet.com.br/busca/passagens/voos#/SAO/BPS/RT/01-09-2019/07-09-2019/-/-/-/1/0/0/-/-/BRL/-" class="btn btn-primary">Comprar agora!</a>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+	<br>
+	<h3>Agência de Viagens Online</h3>
+	<br>
+		<div class="row">
+			<div class="col">
+			<h5><a href="#" >ViajaNet</a></h5>
+				<a href="#" >Passagens Aéreas</a>
+				<a href="#" >Pacotes de viagens</a>
+				<a href="#" >Hotéis</a>
+				<a href="#" >Aplicativo ViajaNet</a>
+				<a href="#" >Blog do ViajaNet</a>
+			</div>
+			<div class="col">
+			<h5><a href="#" >Linhas Aéreas Nacionais</a></h5>
+				<a href="#" >Gol</a>
+				<a href="#" >Azul</a>
+				<a href="#" >Tam agora é Latam</a>
+				<a href="#" >Avianca</a>
+				<a href="#" >Passaredo</a>
+			</div>
+			<div class="col">
+			<h5><a href="#" >Linhas Aéreas Internacionais</a></h5>
+				<a href="#" >Aerolíneas Argentinas</a>
+				<a href="#" >Air Canada</a>
+				<a href="#" >American Airlines</a>
+				<a href="#" >Copa Airlines</a>
+				<a href="#" >Delta Air Lines</a>
+			</div>
+			<div class="col">
+			<h5><a href="#" >Passagens Aéreas Nacionais</a></h5>
+				<a href="#" >Passagem para São Paulo</a>
+				<a href="#" >Passagem para Fortaleza</a>
+				<a href="#" >Passagem para Salvador</a>
+				<a href="#" >Passagem para Curitiba</a>
+				<a href="#" >Passagem Aérea Rio de Janeiro</a>
+			</div>
+			<div class="col">
+				<h5><a href="#" >Passagens Aéreas Internacionais</a></h5>
+				<a href="#" >Passagem para Portugal</a>
+				<a href="#" >Passagem para Orlando</a>
+				<a href="#" >Passagem para Cancun</a>
+				<a href="#" >Passagem para Londres</a>
+				<a href="#" >Passagem para Estados Unidos</a>
+			</div>
+			<div class="col">
+			<h5><a href="#" >QuandoViajar</a></h5>
+				<a href="#" >Voos para o Rio de Janeiro</a>
+				<a href="#" >Voos para Salvador</a>
+				<a href="#" >Voos para Fortaleza</a>
+				<a href="#" >Voos para Miami</a><br>
+				<a href="#" >Voos para Buenos Aires</a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container agViagens2">
+	<br>
+	<hr>
+		<div class="row">
+			<div class="col">
+				<img src="https://www.viajanet.com.br/static/assets/platform/img/newfooter/selo-iata.svg;wa-prodcda429d89011a931">
+			</div>
+			<div class="col">
+				<img src="https://www.viajanet.com.br/static/assets/platform/img/newfooter/selo-brasil.svg;wa-prod38cf373f3e9396cb">
+			</div>
+			<div class="col">
+				<img src="https://www.viajanet.com.br/static/assets/platform/img/newfooter/selo-abav.svg;wa-prod794fc69c05af3d25">
+			</div>
+			<div class="col">
+				<img src="https://www.viajanet.com.br/static/assets/platform/img/newfooter/ico-mob.png;wa-prod289610c6177730b9">
+			</div>
+			<div class="col">
+				<img src="https://www.viajanet.com.br/static/assets/platform/img/newfooter/selo-google-play.png;wa-prod2793b71f2a1f0350">
+			</div>
+			<div class="col">
+				<img src="https://www.viajanet.com.br/static/assets/platform/img/newfooter/selo-app-store.png;wa-proda477e5aab7a4f583">
+			</div>
+		</div>
+	<hr>
+	<br>		
+	</div>
+	
+	<footer class="container">
+		<h6>TVLX Viagens e Turismo S.A. CNPJ: | 12.337.454/0001-31 | Rua Manoel Coelho, 600 - 1º andar - São Caetano do Sul- SP - CEP 09510-101 <br>© 2019 ViajaNet Viagens Online - Todos os direitos reservados </h6> 
+	</footer>
+	
+	<script>
+		function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+	</script>
+	
+	<script src="assets/bootstrap.js"></script> 
+	<script src="assets/bootstrap.min.js"></script>
+	<script src="assets/bootstrap.bundle.min.js"></script>
+	<script src="assets/bootstrap.bundle.js"></script>
+	
+</body>
+</html>
